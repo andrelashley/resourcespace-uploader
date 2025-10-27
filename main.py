268e415b14bc11740ad0beeed5074bd7989e2dcd
André Lashley <andre.lashley@gmail.com>
2025-10-27 14:26:59 -0700

    Add environment variables

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 import hashlib
+import os
+from dotenv import load_dotenv
 
-BASE_URL = ""
-USER = ""
-PRIVATE_KEY = ""
+load_dotenv()
+
+USER = os.environ.get("USER") or "your_username"
+PRIVATE_KEY = os.environ.get("PRIVATE_KEY") or 'placeholder'
 # RS_RESOURCE_ID = 123
 # FILE_NAME = "my_upload_file.jpg"
